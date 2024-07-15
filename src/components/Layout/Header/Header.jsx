8c4e89fa514c729c25aa411c176c48693bebae9f
Anıl Tarar <anil_tarar@hotmail.com>
2024-07-15 12:28:30 +0300

    Project files are created. Header.jsx and Header.css are created.

---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <>
+    <div className="headerBar">
+
+    <h1 style={{cursor:"pointer"}}>Blog Demo w React</h1>
+    <img style={{cursor:"pointer",width:"200px"}} src="https://logos-world.net/wp-content/uploads/2021/08/Blogger-Logo.png" />
+    </div>
+    
+    
+    </>
+  )
+}
+
+export default Header
