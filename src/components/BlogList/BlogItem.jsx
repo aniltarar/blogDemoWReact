3f0f327ce6831e37f0e3337f18bf4e62da805d27
Anıl Tarar <anil_tarar@hotmail.com>
2024-07-16 16:12:55 +0300

    Button Component's propTypes settings are completed.

--- src/components/BlogList/BlogItem.jsx
+++ src/components/BlogList/BlogItem.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import "./BlogItem.css";
 import Button from "../UI/Button";
-const BlogItem = ({id,authorName,authorImage,blogTitle,blogContent,blogCategory,blogDate}) => {
+const BlogItem = ({
+  id,
+  authorName,
+  authorImage,
+  blogTitle,
+  blogContent,
+  blogCategory,
+  blogDate,
+}) => {
   return (
     <>
       <div className="blogItemCard">
         <div className="blogCardHeader">
-          <img className="authorImage" src={authorImage} alt={ `${authorImage}'s image.`} />
-          <h3>{authorName.slice(0,20)}</h3>
+          <img
+            className="authorImage"
+            src={authorImage}
+            alt={`${authorImage}'s image.`}
+          />
+          <h3>{authorName.slice(0, 20)}</h3>
         </div>
-        
+
         <div className="blogCardContent">
-            <h4>{blogTitle}</h4>
-          <p>
-            {blogContent}
-          </p>
+          <h4>{blogTitle}</h4>
+          <p>{blogContent}</p>
         </div>
         <div className="blogCardFooter">
           <div className="leftFooter">
@@ -23,7 +33,6 @@ const BlogItem = ({id,authorName,authorImage,blogTitle,blogContent,blogCategory,
           </div>
           <div className="rigthFooter">
             <Button btnColor={"warning"}>Düzenle</Button>
-            
           </div>
         </div>
       </div>
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import PropTypes from "prop-types";
 import "./Button.css"
 const Button = ({className,btnColor,btnSize,children,onClick}) => {
   return (
     <button onClick={onClick} className={` btn ${className} ${btnColor===""?"btn":btnColor} ${btnSize}`} >{children}</button>
   )
+}
+
+Button.propTypes = {
+    btnColor:PropTypes.oneOf(["primary","secondary","danger","success","dark","warning"]),
+    btnSize:PropTypes.oneOf(["sm","md","lg"]),
+    onClick:PropTypes.func,
 }
 
 export default Button
